648f8f9df010c749b5eac40f8fa66ca41bd9a26a
Aoi <ht.so.ik0102@gmail.com>
2021-08-03 23:18:31 +0700

    control key

--- src/components/ControlKey/DestroyControlKey.js
+++ src/components/ControlKey/DestroyControlKey.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputLabel, TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Container, Grid } from '@material-ui/core';
+import Web3 from "web3";
+
+import { CONTROLKEY_ABI, CONTROLKEY_ADDRESS } from "../../controlKeyConfig";
 
 const useDestroykeyStyles = makeStyles((theme) => ({
   pageTitle: {
@@ -42,6 +45,31 @@ const useDestroykeyStyles = makeStyles((theme) => ({
 export default function DestroyControlKey() {
   const classes = useDestroykeyStyles();
 
+  const [idNumber, setIdNumber] = useState("");
+
+  const handleDestroy = async () => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const accounts = await web3.eth.getAccounts();
+    const selectedAccount = window.ethereum.selectedAddress;
+    let config = {
+      from: accounts[0],
+    };
+    const contractInstance = new web3.eth.Contract(CONTROLKEY_ABI, CONTROLKEY_ADDRESS, {
+      from: accounts[0],
+    });
+    contractInstance.methods
+      .destroyControlKey(idNumber)
+      .send(config)
+      .on('receipt', (res) => {
+        console.log(res);
+      })
+      .on('error', (error) => {
+        alert("Can not destroy because the token is not burnable")
+      });
+
+    setIdNumber("");
+  }
+
   return (
     <div>
       <div className={classes.pageTitle}>Destroy Control Key</div>
@@ -52,10 +80,10 @@ export default function DestroyControlKey() {
               <InputLabel className={classes.label}>Enter the identification number (the wallet addres) for the dtrust.</InputLabel>
             </Grid>
             <Grid item xs={8} sm={4}>
-              <TextField className={classes.input} label="ID number" id="" variant="outlined" size="small" />
+              <TextField className={classes.input} label="ID number" id="" variant="outlined" size="small" value={idNumber} onChange={(e) => setIdNumber(e.target.value)} />
             </Grid>
             <Grid item xs={4} sm={1}>
-              <Button className={classes.button}>Enter</Button>
+              <Button className={classes.button} onClick={handleDestroy}>Enter</Button>
             </Grid>
           </Grid>
         </form>
