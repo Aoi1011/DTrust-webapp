071dc28230622ff2ceb8fd605cca0e11ee364ab3
Aoi <ht.so.ik0102@gmail.com>
2021-08-28 10:53:44 +0700

    add table

--- src/components/ControlKey/UseControlKey.js
+++ src/components/ControlKey/UseControlKey.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { InputLabel, TextField } from '@material-ui/core';
+import { InputLabel, TextField, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Container, Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 
 const useUsekeyStyles = makeStyles((theme) => ({
   pageTitle: {
@@ -10,7 +11,7 @@ const useUsekeyStyles = makeStyles((theme) => ({
   },
   container: {
     paddingTop: '5vh',
-    paddingBottom: '30vh',
+    paddingBottom: '5vh',
     '& .MuiGrid-item': {
       display: 'flex',
       alignItems: 'center',
@@ -37,7 +38,28 @@ const useUsekeyStyles = makeStyles((theme) => ({
   input: {
     width: '100%',
   },
+  tableContainer: {
+    paddingBottom: '5vh',
+  },
+  table: {
+    minWidth: 650,
+  },
 }));
+
+function createData(name, calories) {
+  return { name, calories };
+}
+
+const rows = [
+  createData(`Settlor's beneficiary address key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Trustee's beneficiary address key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Settlor's distribution schedule key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Trustee's distribution schedule key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Settlor's revocation key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Trustee's revocation key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Settlor's asset swap key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+  createData(`Trustee's transaction key`, 0x71C7656EC7ab88b098defB751B7401B5f6d8976F),
+];
 
 export default function UseControlKey() {
   const classes = useUsekeyStyles();
@@ -60,6 +82,32 @@ export default function UseControlKey() {
           </Grid>
         </form>
       </Container>
+      <TableContainer component={Paper} className={classes.tableContainer}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Relevant control keys</TableCell>
+              <TableCell align="center">Detail</TableCell>
+              {/* <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row" align="center">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.calories}</TableCell>
+                {/* <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell> */}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
