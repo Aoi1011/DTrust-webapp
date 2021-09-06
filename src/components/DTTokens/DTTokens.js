f735c435068c78b89b79756aaa7c59509843d85a
zwecken <85690546+zwecken@users.noreply.github.com>
2021-09-06 11:56:51 +0400

    Update DTTokens.js

--- src/components/DTTokens/DTTokens.js
+++ src/components/DTTokens/DTTokens.js
@@ -28,8 +28,8 @@ export default function DTTokens() {
     <div className={classes.root}>
       <div className={classes.pageTitle}>Gov</div>
       <Container>
-        <div className={classes.content}>DTrust takes 0.5% of all assets held in dtrusts each year in semiannual charges. If a promoter arranged the dtrust, then fees for the first two years go to the promoter and all subsequent fees go to DT tokens. If there was no promoter, then all fees go to DT tokens. The fees are split evenly among 1,000 DT tokens. There will never be any more DT tokens.</div>
-        <div className={classes.content}>Example: If there are assets worth $10 million in all the dtrusts, then all the DT tokens would receive assets worth $50,000 in aggregate each year (two $25,000 distributions). Each DT token would receive $50 worth of assets (two $25 distributions).</div>
+        <div className={classes.content}>The DTrust protocol is governed by a DAO made up of DT Tokens. DT Tokens vote and earn fees. Votes to change the protocol require more than 2/3 approval. DTrust takes 0.5% of all assets held in dtrusts each year in semiannual charges, which are paid out proportionally to the wallets that hold DT tokens. If a promoter arranged the dtrust, then fees for the first two years go to the promoter and all subsequent fees go to DT tokens. If there was no promoter, then all fees go to DT token holders. NFTs and other non-divisible assets do not pay fees. </div>
+        <div className={classes.content}>Example: If a particular dtrust holds $10,000,000 worth of assets during a year, then the total fees paid that year would be $50,000 (two $25,000 distributions). Each DT token would receive $50 worth of assets (two $25 distributions).</div>
         <div className={classes.content}>If you are interested in purchasing DT tokens, fill out the contact form with the button at the bottom of the page.</div>
       </Container>
     </div>
