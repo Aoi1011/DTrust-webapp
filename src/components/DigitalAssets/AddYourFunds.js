6ea53eb03dbad45a674d74b588985eea8d8e1a49
zwecken <85690546+zwecken@users.noreply.github.com>
2021-09-15 17:54:12 +0400

    Update AddYourFunds.js

--- src/components/DigitalAssets/AddYourFunds.js
+++ src/components/DigitalAssets/AddYourFunds.js
@@ -56,73 +56,73 @@ function AddYourFunds() {
       {inputList.map((x, i) => {
         return (
             <p className="form-container_label">Digital asset {i + 1}</p>
-            <>
-              <div className="form-container_inputs selectParent">
-                <>
-                  <select
+              <>
+                <div className="form-container_inputs selectParent">
+                  <>
+                    <select
+                      className="form-container_input_item"
+                      name="text"
+                      placeholder="Enter First Name"
+                      value={x.text}
+                      required
+                      onChange={(e) => handleInputChange(e, i)}
+                    >
+                      <option>{'Digital asset'}</option>
+                      <option>ETH</option>
+                      <option>USDT</option>
+                      <option>wBTC</option>
+                      <option>wXMR</option>
+                      <option>USDC</option>
+                      <option>DAI</option>
+                      <option>UNI</option>
+                      <option>SUSHI</option>
+                      <option>renDOGE</option>
+                      <option>ENJ</option>
+                      <option>SAND</option>
+                      <option>ANT</option>
+                      <option>CLV</option>
+                      <option>NWC</option>
+                      <option>CHR</option>
+                      <option>LIT</option>
+                      <option>CFG</option>
+                      <option>DVI</option>
+                      <option>NTF token</option>
+                    </select>
+                  </>
+                  <input
                     className="form-container_input_item"
-                    name="text"
-                    placeholder="Enter First Name"
-                    value={x.text}
+                    name="amount"
+                    placeholder="Amount"
+                    value={x.amount}
                     required
                     onChange={(e) => handleInputChange(e, i)}
-                  >
-                    <option>{'Digital asset'}</option>
-                    <option>ETH</option>
-                    <option>USDT</option>
-                    <option>wBTC</option>
-                    <option>wXMR</option>
-                    <option>USDC</option>
-                    <option>DAI</option>
-                    <option>UNI</option>
-                    <option>SUSHI</option>
-                    <option>renDOGE</option>
-                    <option>ENJ</option>
-                    <option>SAND</option>
-                    <option>ANT</option>
-                    <option>CLV</option>
-                    <option>NWC</option>
-                    <option>CHR</option>
-                    <option>LIT</option>
-                    <option>CFG</option>
-                    <option>DVI</option>
-                    <option>NTF token</option>
-                  </select>
-                </>
-                <input
-                  className="form-container_input_item"
-                  name="amount"
-                  placeholder="Amount"
-                  value={x.amount}
-                  required
-                  onChange={(e) => handleInputChange(e, i)}
-                />
-              </div>
-              <>
-                <div> className={x.token ? 'active_ntf' : 'ntf-blcok'}>
-                  <input
-                  className="form-container_input_item"
-                  name="nft_unique_ID_code"
-                  placeholder="NFT Unique Identification Code"
-                  value={x.amount}
-                  required
-                  onChange={(e) => handleInputChange(e, i)}
                   />
                 </div>
-                <div> className="btn-box form-container_add">
-                  {inputList.length - 1 === i && (
-                    <button
-                      className="form-container_add_btn"
-                      onClick={handleAddClick}
-                    >
-                      Add new
-                    </button>
-                  )}
-                </div>
-              </>  
-            </>
-        )
-      }  
+                <>
+                  <div> className={x.token ? 'active_ntf' : 'ntf-blcok'}>
+                    <input
+                    className="form-container_input_item"
+                    name="nft_unique_ID_code"
+                    placeholder="NFT Unique Identification Code"
+                    value={x.amount}
+                    required
+                    onChange={(e) => handleInputChange(e, i)}
+                    />
+                  </div>
+                  <div> className="btn-box form-container_add">
+                    {inputList.length - 1 === i && (
+                      <button
+                        className="form-container_add_btn"
+                        onClick={handleAddClick}
+                      >
+                        Add new
+                      </button>
+                    )}
+                  </div>
+                </>  
+              </>
+          )
+        }  
       <div className="save_block">
         <button className="save_block_btn" onClick={handleSubmit} type="submit">
           Save
