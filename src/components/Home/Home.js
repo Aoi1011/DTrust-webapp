f7420be6304cc71d6728257b6621c9220c8f83d3
zwecken <85690546+zwecken@users.noreply.github.com>
2021-09-16 13:47:11 +0400

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -220,27 +220,21 @@ export default function Home() {
         <div>Audited and Verified</div>
         <div>
           DTrust uses proven openzeppelin smart contracts.&nbsp;
-          <Link to={{ pathname: "https://dtrust.notion.site/DTrust-Source-Code-d83a8f5f6b8f4d7e9e03772430703846" }} style={{ textDecoration: 'none' }}>
-               <RouterLink>
+          <Link to="https://dtrust.notion.site/DTrust-Source-Code-d83a8f5f6b8f4d7e9e03772430703846">
                See Contracts
-               </RouterLink>
-            </Link>
+          </Link>
         </div>
         <div>
           DTrust underwent rigorous security analysis.&nbsp;
-          <Link to={{ pathname: "https://dtrust.notion.site/DTrust-Testing-dad9b472ad0743e7b7068fda1ca4b9b2" }} style={{ textDecoration: 'none' }}>
-               <RouterLink>
+          <Link to="https://dtrust.notion.site/DTrust-Testing-dad9b472ad0743e7b7068fda1ca4b9b2">
                See Testing
-               </RouterLink>
            </Link>
         </div>
         <div>
           DTrust received an audit from Hacken.io.&nbsp;
-          <Link to={{ pathname: "https://dtrust.notion.site/DTrust-Audit-bbf4aca8598b4460a78da62cda849eb9" }} style={{ textDecoration: 'none' }}>
-               <RouterLink>
+          <Link to="https://dtrust.notion.site/DTrust-Audit-bbf4aca8598b4460a78da62cda849eb9">
                See Audit
-               </RouterLink>
-           </Link>
+          </Link>
         </div>
         <div className={classes.aboutus}>
           <Link to="/aboutus">
